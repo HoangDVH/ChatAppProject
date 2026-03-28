cf4a12cda1bb2821a3deb2c05224344d4b625b9c
HoangDVH <dauvuhoang01@gmail.com>
2026-03-28 21:56:46 +0700

    update backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,14 @@ import authRoute from "./routes/authRoute.js";
 import userRoute from "./routes/userRoute.js";
 import cookieParser from "cookie-parser";
 import { protectedRoute } from "./middlewares/authMiddleware.js";
+
+import friendRoute from "./routes/friendRoute.js";
+import messageRoute from "./routes/messageRoute.js";
+import conversationRoute from "./routes/conversationRoute.js";
 import cors from "cors";
+
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
@@ -15,12 +22,22 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
+// swagger
+const swaggerDocument = JSON.parse(
+  fs.readFileSync("./src/swagger.json", "utf8"),
+);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 //public routes
 app.use("/api/auth", authRoute);
 
 //private routes
 app.use(protectedRoute);
 app.use("/api/users", userRoute);
+app.use("/api/friends", friendRoute);
+app.use("/api/messages", messageRoute);
+app.use("/api/conversations", conversationRoute);
 
 connectDB().then(() => {
   app.listen(PORT, () => {
